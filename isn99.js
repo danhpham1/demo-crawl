f259fcaa637146c1d7ef7973f7e155c0588e8234
DanhPhamKyanon <danh.pham@kyanon.digital>
2022-08-16 22:17:08 +0700

    format get data

--- isn99.js
+++ isn99.js
@@ -27,56 +27,96 @@ const delay = (time) => {
 const crawl = async () => {
     const axiosInstance = getAxiosInstance();
     const userInfo = await axiosInstance.post('http://www.isn99.com/membersite-api/api/member/authenticate', {
-        username:"IW8866",
-        password:"Ao7xW3EEsm"
+        username: "IW8866",
+        password: "Ao7xW3EEsm"
     });
-    while (true) {
-        // await delay(20000);
-        if (userInfo.data && userInfo.data.success == true) {
-            const listMatchLivePromise =  axiosInstance.get(`http://www.isn99.com/membersite-api/api/data/events/1/3/0/7/3?_=${new Date().getTime()}`,{ headers: { Authorization:  `Bearer ${userInfo.data.token}`} });
-            const listMatchTodayPromise =  axiosInstance.get(`http://www.isn99.com/membersite-api/api/data/events/1/2/0/7/3?_=${new Date().getTime()}`,{ headers: { Authorization:  `Bearer ${userInfo.data.token}`} });
-            const [listMatchLive, listMatchToday] = await Promise.allSettled([listMatchLivePromise, listMatchTodayPromise]);
-            let totalListMatch = [];
-            if(listMatchLive.status == 'fulfilled'){
-                totalListMatch = [...totalListMatch, ...listMatchLive.value.data.schedule.leagues]
-            }
-            if(listMatchToday.status == 'fulfilled'){
-                totalListMatch = [...totalListMatch, listMatchToday.value.data.schedule.leagues]
-            }
-            const listTransferDataMatch = totalListMatch.reduce((temp,leadgues) => {
-                const matchOfleagues = [];
-                leadgues.events?.map(match => {
-                    const markets = match.markets.reduce((temp, market) => {
-                        const transferLines = market.lines.reduce((temp, line) => {
-                            const transferMarket = line.marketSelections.map(value => {
-                                return {
-                                    odds: value.odds,
-                                    handicap: value.handicap,
-                                    decimalOdds: value.decimalOdds
-                                }
-                            });
-                            temp = [...temp, {marketSelections: transferMarket}];
-                            return temp;
-                        },[])
-                        temp = [...temp, {
-                            [`${market.name}`]: transferLines
-                        }]
-                        return temp;
-                    },[])
-                    matchOfleagues.push({
-                        homeTeam: match.homeTeam,
-                        awayTeam: match.awayTeam,
-                        score: match.score,
-                        markets: markets
-                    })
-                })
-                temp = [...temp, ...matchOfleagues];
-                return temp;
-            }, [])
-            fs.writeFileSync('isn99.json', JSON.stringify(listTransferDataMatch),'utf8');
-            console.log('done')
+    // while (true) {
+    // await delay(20000);
+    if (userInfo.data && userInfo.data.success == true) {
+        const listMatchLivePromise = axiosInstance.get(`http://www.isn99.com/membersite-api/api/data/events/1/3/0/7/3?_=${new Date().getTime()}`, { headers: { Authorization: `Bearer ${userInfo.data.token}` } });
+        const listMatchTodayPromise = axiosInstance.get(`http://www.isn99.com/membersite-api/api/data/events/1/2/0/7/3?_=${new Date().getTime()}`, { headers: { Authorization: `Bearer ${userInfo.data.token}` } });
+        const [listMatchLive, listMatchToday] = await Promise.allSettled([listMatchLivePromise, listMatchTodayPromise]);
+        let totalListMatch = [];
+        if (listMatchLive.status == 'fulfilled') {
+            totalListMatch = [...totalListMatch, ...listMatchLive.value.data.schedule.leagues]
         }
+        if (listMatchToday.status == 'fulfilled') {
+            totalListMatch = [...totalListMatch, ...listMatchToday.value.data.schedule.leagues]
+        }
+        const infoAllMatch = [];
+        totalListMatch.map(league => {
+            league.events.map(match => {
+                const infoMatch = {};
+                infoMatch['infomatch'] = {
+                    namehome: match.homeTeam ? match.homeTeam : null,
+                    nameaway: match.awayTeam ? match.awayTeam : null,
+                    score: match.score ? match.score : null,
+                    time: match.period ? match.period : null
+                }
+                infoMatch['fullmatch'] = {};
+
+                infoMatch['fullmatch']['1X2'] = {};
+                infoMatch['fullmatch']['hdp'] = [];
+                infoMatch['fullmatch']['ou'] = [];
+
+                infoMatch['firstmatch'] = {};
+                infoMatch['firstmatch']['1X2'] = {};
+                infoMatch['firstmatch']['hdp'] = [];
+                infoMatch['firstmatch']['ou'] = [];
+
+                match?.markets.map(market => {
+                    if (market.name == '1x2') {
+                        market.selections.map((selection) => {
+                            if (selection.indicator == 'Home') {
+                                infoMatch['fullmatch']['1X2']['1'] = selection.odds
+                            }
+                            if (selection.indicator == 'Away') {
+                                infoMatch['fullmatch']['1X2']['2'] = selection.odds
+                            }
+                            if (selection.indicator == 'Draw') {
+                                infoMatch['fullmatch']['1X2']['X'] = selection.odds
+                            }
+                        })
+                    }
+                    if (market.name == 'HDP' || market.name == 'HT HDP') {
+                        market.lines.map((line) => {
+                            infoMatch['fullmatch']['hdp'].push({
+                                goal: line.marketSelections[0].handicap.replace(/[+\/*-]/g, ' ').replace(/ /g,''),
+                                oddhome: line.marketSelections[0].odds,
+                                oddaway: line.marketSelections[1].odds
+                            });
+                        });
+                    }
+                    if (market.name == 'OU' || market.name == 'HT OU') {
+                        market.lines.map((line) => {
+                            infoMatch['fullmatch']['ou'].push({
+                                goal: line.marketSelections[0].handicap.replace(/[+\/*-]/g, ' ').replace(/ /g,''),
+                                oddhome: line.marketSelections[0].odds,
+                                oddaway: line.marketSelections[1].odds
+                            });
+                        });
+                    }
+                    if (market.name == 'HT 1x2'){
+                        market.selections.map((selection) => {
+                            if (selection.indicator == 'Home') {
+                                infoMatch['firstmatch']['1X2']['1'] = selection.odds
+                            }
+                            if (selection.indicator == 'Away') {
+                                infoMatch['firstmatch']['1X2']['2'] = selection.odds
+                            }
+                            if (selection.indicator == 'Draw') {
+                                infoMatch['firstmatch']['1X2']['X'] = selection.odds
+                            }
+                        })
+                    }
+                })
+                infoAllMatch.push(infoMatch);
+            })
+        })
+        fs.writeFileSync('isn99.json', JSON.stringify(infoAllMatch), 'utf8');
+        console.log('done')
     }
+    // }
 }
 
 const runCrawl = async () => {
@@ -84,7 +124,7 @@ const runCrawl = async () => {
         await crawl();
     } catch (error) {
         console.log(error)
-        runCrawl();
+        // runCrawl();
     }
 }
 
